e4dd70145a551f3300618e014ab87a9d1035a344
Brunodias101 <bruno101dias@gmail.com>
2025-08-12 23:36:20 -0300

    primeiro commit

---
+++ catalagofilmes/src/main/java/com/bruno/catalagofilmes/CatalagofilmesApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.catalagofilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalagofilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalagofilmesApplication.class, args);
+	}
+
+}
---
+++ catalagofilmes/src/test/java/com/bruno/catalagofilmes/CatalagofilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.catalagofilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalagofilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
